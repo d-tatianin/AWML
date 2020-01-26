406ecb462671d7c59680c74cb8ed2b0edad509a2
8infy <99danilt@gmail.com>
2020-01-26 17:55:18 +0300

    Slightly refactored the dependency_downloader script.

--- dependency_downloader.py
+++ dependency_downloader.py
@@ -3,7 +3,7 @@ import os
 import urllib.request
 
 def error_exit(why):
-   print("-- Failed to install OpenGL dependencies! Reason: {}".format(why))
+   print(f"-- Failed to install OpenGL dependencies! Reason: {why}")
    sys.exit(1)
 
 if len(sys.argv) != 2:
@@ -11,18 +11,17 @@ if len(sys.argv) != 2:
          \nPlease note that this script is meant to be executed with CMake and not manually!")
     sys.exit(1)
 if not os.path.isdir(sys.argv[1]):
-    error_exit("no such directory: {}".format(sys.argv[1]))
+    error_exit(f"no such directory: {sys.argv[1]}")
 try:
     print("-- Downloading OpenGL dependencies...")
-    gl_path = sys.argv[1] + "/GL"
-    khr_path = sys.argv[1] + "/KHR"
+    gl_path = os.path.join(sys.argv[1], "GL")
+    khr_path = os.path.join(sys.argv[1], "KHR")
     os.mkdir(gl_path)
     os.mkdir(khr_path)
     urllib.request.urlretrieve("https://www.khronos.org/registry/OpenGL/api/GL/glcorearb.h", gl_path + "/glcorearb.h")
     urllib.request.urlretrieve("https://www.khronos.org/registry/OpenGL/api/GL/wglext.h", gl_path + "/wglext.h")
     urllib.request.urlretrieve("https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h", khr_path + "/khrplatform.h")
 except Exception as ex:
-    error_exit(ex)    
+    error_exit(ex)
 
 print("-- Successfully downloaded OpenGL dependencies.")
-sys.exit(0)
